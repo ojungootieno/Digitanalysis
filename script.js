2a29c59a412684834d1c004903b332934d0388d5
ojungootieno <ojungootieno@gmail.com>
2026-07-20 00:55:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 const digitElement = document.getElementById("digit");
 const historyElement = document.getElementById("history");
+const marketSelect = document.getElementById("market");
 
 let history = [];
 let counts = {
-  0:0,1:0,2:0,3:0,4:0,
-  5:0,6:0,7:0,8:0,9:0
+    0:0,1:0,2:0,3:0,4:0,
+    5:0,6:0,7:0,8:0,9:0
 };
 
-function analyzeDigit(digit){
+function resetData() {
+    history = [];
+    counts = {
+        0:0,1:0,2:0,3:0,4:0,
+        5:0,6:0,7:0,8:0,9:0
+    };
 
-    digitElement.textContent = digit;
-history.unshift(digit);
+    historyElement.textContent = "";
 
-if(history.length > 20){
-    history.pop();
+    for (let i = 0; i <= 9; i++) {
+        document.getElementById("d" + i).textContent = "0";
+    }
 }
 
-historyElement.textContent = history.join(" ");
+function analyzeDigit(digit) {
+    digitElement.textContent = digit;
+
+    history.unshift(digit);
+
+    if (history.length > 20) {
+        history.pop();
+    }
+
+    historyElement.textContent = history.join(" ");
+
     counts[digit]++;
 
-    for(let i=0;i<=9;i++){
-        document.getElementById("d"+i).textContent = counts[i];
+    for (let i = 0; i <= 9; i++) {
+        document.getElementById("d" + i).textContent = counts[i];
     }
-
 }
-const testDigits = [7,1,9,5,7,7,3,2,0,8,7,6,1,4,9];
-
-let index = 0;
-
-setInterval(()=>{
-
-    analyzeDigit(testDigits[index]);
-
-    index++;
-
-    if(index >= testDigits.length){
-        index = 0;
-    }
-
-},1000);
