73e969fba0b27b7aced276124b60877d397b5d5c
ojungootieno <ojungootieno@gmail.com>
2026-07-20 02:07:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -39,35 +39,7 @@ function analyzeDigit(digit) {
         document.getElementById("d" + i).textContent = counts[i];
     }
 }
-const testDigits = [7,1,9,5,7,7,3,2,0,8,7,6,1,4,9];
 
-let index = 0;
-
-setInterval(() => {
-const connection = new WebSocket(
-    "wss://ws.derivws.com/websockets/v3?app_id=33ShJudJnwVSh7EiKMdyI"
-);
-
-connection.onopen = () => {
-    console.log("Connected to Deriv");
-};
-
-connection.onerror = (error) => {
-    console.log("Connection error", error);
-};
-
-connection.onclose = () => {
-    console.log("Connection closed");
-};
-    analyzeDigit(testDigits[index]);
-
-    index++;
-
-    if (index >= testDigits.length) {
-        index = 0;
-    }
-
-}, 1000);
 connection.onmessage = (event) => {
 
     const data = JSON.parse(event.data);
