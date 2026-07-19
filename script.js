a0ff2230bc0e001cdf6251b9f6239aead9cef5e5
ojungootieno <ojungootieno@gmail.com>
2026-07-19 20:03:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ function analyzeDigit(digit){
     }
 
 }
+const testDigits = [7,1,9,5,7,7,3,2,0,8,7,6,1,4,9];
+
+let index = 0;
+
+setInterval(()=>{
+
+    analyzeDigit(testDigits[index]);
+
+    index++;
+
+    if(index >= testDigits.length){
+        index = 0;
+    }
+
+},1000);
