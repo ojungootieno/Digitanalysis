e8e1989f7b1acf828dd2d723a329c8ca8bff8b39
ojungootieno <ojungootieno@gmail.com>
2026-07-22 23:31:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -46,25 +46,7 @@ function analyzeDigit(digit) {
     }
 }
 
-const testDigits = [
-    7,1,9,5,7,7,3,2,0,8,
-    7,6,1,4,9,0,2,5,8,3,
-    4,7,1,6,9,5,0,8,2,7
-];
 
-let index = 0;
-
-setInterval(() => {
-
-    analyzeDigit(testDigits[index]);
-
-    index++;
-
-    if (index >= testDigits.length) {
-        index = 0;
-    }
-
-}, 1000);
 
 marketSelect.addEventListener("change", () => {
     resetData();
